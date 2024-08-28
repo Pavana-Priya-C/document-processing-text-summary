c84b92a2989eb7a4b05e38b3978b4979c46ac101
Pavana <pavanapc.riverdale@gmail.com>
2024-08-28 21:03:32 +0530

    adding new files

---
+++ preprocess.py
@@ -0,0 +1,76 @@
+# preprocess.py
+import re
+from pdfminer.high_level import extract_text
+
+# Function to extract text from PDF using PDFMiner
+def extract_text_from_pdf(pdf_path):
+    return extract_text(pdf_path)
+
+# Function to get the title
+def get_title(text):
+    match = re.search(r"\d+\s*\n\n(.*?)\n", text, re.DOTALL)
+    if match:
+        return match.group(1).strip()
+    else:
+        return None
+
+#The below function is used only for Chapter 8
+def preprocess_chap8(raw_text):
+  pattern = r'^.*?(\d+).*?READ AND FIND OUT'
+
+  # Search for the pattern and get the match
+  match = re.search(pattern, raw_text, re.DOTALL)
+
+  if match:
+      # Extract the first occurrence of the number
+      number_before_read = match.group(1)
+      # Remove everything before the number and the "READ AND FIND OUT" section
+      cleaned_text = re.sub(pattern, number_before_read, raw_text, flags=re.DOTALL)
+  else:
+      cleaned_text = raw_text
+
+  return cleaned_text
+
+
+# Function for preprocessing text
+def preprocess_text(text):
+    processed_text = re.sub(r'Reprint 2024-25|\d|\d+\s+Footprints without Feet|Footprints without Feet|A Triumph of Surgery\s\d|READ AND FIND OUT[\s\S]*?(?:•\s.*\n)+', '', text)
+  
+    # Replace multiple spaces with a single space
+    processed_text = re.sub(r'\s+', ' ', processed_text)
+
+    author_pattern = r'\b[A-Z]+\s+[A-Z]+\b'
+    processed_text = re.sub(author_pattern, '', processed_text)
+
+    # Strip leading and trailing whitespace
+    processed_text = processed_text.strip()
+
+    return processed_text
+
+# Function to remove multiple occurrences of the title
+def remove_subsequent_occurrences(text, phrase):
+    parts = text.split(phrase)
+    return phrase + parts[1] + "".join(parts[2:])
+
+# Function to get separate sections from the extracted text
+def separate_sections(text):
+    # Define the regex patterns to extract the sections
+    glossary_pattern = re.compile(r'GLOSSARY(.*?)Think about it', re.DOTALL)
+    think_about_it_pattern = re.compile(r'Think about it(.*?)Talk about it', re.DOTALL)
+    talk_about_it_pattern = re.compile(r'Talk about it(.*?)Suggested reading', re.DOTALL)
+    suggested_reading_pattern = re.compile(r'Suggested reading(.*)', re.DOTALL)
+
+    # Extract the sections using the patterns
+    main_content = re.split(r'GLOSSARY|Think about it|Talk about it|Suggested reading', text)[0].strip()
+    glossary = glossary_pattern.search(text)
+    think_about_it = think_about_it_pattern.search(text)
+    talk_about_it = talk_about_it_pattern.search(text)
+    suggested_reading = suggested_reading_pattern.search(text)
+
+    glossary = glossary.group(1).strip() if glossary else ''
+    think_about_it = think_about_it.group(1).strip() if think_about_it else ''
+    talk_about_it = talk_about_it.group(1).strip() if talk_about_it else ''
+    suggested_reading = suggested_reading.group(1).strip() if suggested_reading else ''
+
+    # Return the extracted sections
+    return main_content, glossary, think_about_it, talk_about_it, suggested_reading
---
+++ summarization.py
@@ -0,0 +1,53 @@
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from summarizer import Summarizer
+from transformers import BartForConditionalGeneration, BartTokenizer, T5ForConditionalGeneration, T5Tokenizer
+
+# Function to summarize text with Sumy (extractive)
+def summarize_text_sumy(text, sentences_count=8):
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LexRankSummarizer()
+    summary = summarizer(parser.document, sentences_count)
+    return " ".join([str(sentence) for sentence in summary])
+
+# Function to summarize text using BERT Transformer
+def summarize_text_bert(main_content):
+    model = Summarizer('distilbert-base-uncased', hidden=[-1,-2], hidden_concat=True)
+    summary = model(main_content, num_sentences=7)
+    return summary
+
+# Function to summarize text using BART Transformer
+def summarize_text_bart(main_content):
+    bart_model_name = "facebook/bart-large-cnn"
+    bart_tokenizer = BartTokenizer.from_pretrained(bart_model_name)
+    bart_model = BartForConditionalGeneration.from_pretrained(bart_model_name)
+
+    bart_inputs = bart_tokenizer([main_content], max_length=1024, return_tensors='pt', truncation=True)
+    bart_summary_ids = bart_model.generate(bart_inputs['input_ids'], max_length=250, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = bart_tokenizer.decode(bart_summary_ids[0], skip_special_tokens=True)
+    return summary
+
+# Function to summarize text using T5 Transformer (large)
+def summarize_text_t5_large(main_content):
+    t5_model_name = "t5-large"
+    t5_tokenizer = T5Tokenizer.from_pretrained(t5_model_name)
+    t5_model = T5ForConditionalGeneration.from_pretrained(t5_model_name)
+
+    input_text = "summarize: " + main_content
+    t5_inputs = t5_tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
+    t5_summary_ids = t5_model.generate(t5_inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = t5_tokenizer.decode(t5_summary_ids[0], skip_special_tokens=True)
+    return summary
+
+# Function to summarize text using T5 Transformer (base)
+def summarize_text_t5_base(main_content):
+    t5_base_model_name = "t5-base"
+    t5_base_tokenizer = T5Tokenizer.from_pretrained(t5_base_model_name)
+    t5_base_model = T5ForConditionalGeneration.from_pretrained(t5_base_model_name)
+
+    base_input_text = "summarize: " + main_content
+    t5_base_inputs = t5_base_tokenizer.encode(base_input_text, return_tensors="pt", max_length=512, truncation=True)
+    t5_base_summary_ids = t5_base_model.generate(t5_base_inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = t5_base_tokenizer.decode(t5_base_summary_ids[0], skip_special_tokens=True)
+    return summary
