6d705dab8859290e2aefab49e98ac1c067d3f198
Pavana <pavanapc.riverdale@gmail.com>
2024-08-28 22:30:00 +0530

    nltk data

---
+++ download_nltk_data.py
@@ -0,0 +1,5 @@
+import nltk
+
+# Specify the directory where you want to download the NLTK data
+nltk.download('punkt', download_dir='./nltk_data')
+nltk.download('stopwords', download_dir='./nltk_data')
